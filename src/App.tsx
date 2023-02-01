5e0204f722567f34a4744581f8a2e5e307969573
ablfaxl <taghavi.abolfazl@outlook.com>
2023-02-01 15:38:17 +0330

    smthing

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import { useSelector } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import AdminRoutes from "./routes/AdminRoutes";
-import EmployerRoutes from "./routes/EmployerRoutes";
-import JobSeekerRoutes from "./routes/JobSeekerRoutes";
-import UiRoutes from "./routes/UiRoutes";
+import { useSelector } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import AdminRoutes from './routes/AdminRoutes';
+import EmployerRoutes from './routes/EmployerRoutes';
+import JobSeekerRoutes from './routes/JobSeekerRoutes';
+import UiRoutes from './routes/UiRoutes';
 
 function App() {
-    const users = useSelector((state: any) => state.usersSlice.currentUser);
-    const employers = useSelector(
-        (state: any) => state.employersSlice.currentEmployer
-    );
+  const users = useSelector((state: any) => state.usersSlice.currentUser);
+  const employers = useSelector(
+    (state: any) => state.employersSlice.currentEmployer
+  );
 
-    // console.log(users);
-    // console.log(employers);
+  console.log(users);
+  console.log(employers);
 
-    return (
-        <BrowserRouter>
-            <AdminRoutes />
-            <UiRoutes />
-            <JobSeekerRoutes />
-            <EmployerRoutes />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <AdminRoutes />
+      <UiRoutes />
+      <JobSeekerRoutes />
+      <EmployerRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
